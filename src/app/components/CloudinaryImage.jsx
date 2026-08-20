7a34a27f76564a8a0a978d38fa44606064df54f9
Lavanya-Sinha <lavanyasinha400@gmail>
2026-08-21 03:56:11 +0530

    Configure Cloudinary client images

--- src/app/components/CloudinaryImage.jsx
+++ src/app/components/CloudinaryImage.jsx
@@ -1,7 +1,12 @@
-"use client"
+"use client";
 
 import { CldImage } from "next-cloudinary";
 
 export default function CloudinaryImage(props) {
-  return <CldImage {...props} />;
+    return (
+        <CldImage
+            {...props}
+            cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}
+        />
+    );
 }
