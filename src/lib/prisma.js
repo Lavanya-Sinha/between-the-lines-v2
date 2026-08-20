50488c4bf09d03fa2c9087186614693dc22e5af8
Lavanya-Sinha <lavanyasinha400@gmail>
2026-08-21 01:08:56 +0530

    Use Node trusted CA for Aiven PostgreSQL

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,25 +1,8 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import fs from "fs";
-
-let ssl;
-
-try {
-    ssl = {
-        ca: fs.readFileSync(
-            "/etc/secrets/aiven-ca.pem",
-            "utf8"
-        ),
-        rejectUnauthorized: true,
-    };
-} catch {
-    ssl = undefined;
-}
 
 const adapter = new PrismaPg({
-    connectionString:
-        process.env.DATABASE_URL,
-    ...(ssl && { ssl }),
+    connectionString: process.env.DATABASE_URL,
 });
 
 const prisma = new PrismaClient({
