38544df1bb2c43e41e15867de415e7130891766b
Lavanya-Sinha <lavanyasinha400@gmail>
2026-08-21 02:05:39 +0530

    Fix Aiven PostgreSQL SSL compatibility

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,19 +1,8 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import fs from "fs";
-
-const caPath = "/etc/secrets/aiven-ca.pem";
-
-const ssl = fs.existsSync(caPath)
-    ? {
-          ca: fs.readFileSync(caPath, "utf8"),
-          rejectUnauthorized: true,
-      }
-    : undefined;
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
-    ...(ssl && { ssl }),
 });
 
 const prisma = new PrismaClient({
