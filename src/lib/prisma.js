c724651f7d85feee5e27af83c4549032ee2f9d4c
Lavanya-Sinha <lavanyasinha400@gmail>
2026-08-21 00:56:28 +0530

    Load database CA certificate at runtime

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,15 +2,24 @@ import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import fs from "fs";
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
+let ssl;
+
+try {
+    ssl = {
         ca: fs.readFileSync(
             "/etc/secrets/aiven-ca.pem",
             "utf8"
         ),
         rejectUnauthorized: true,
-    },
+    };
+} catch {
+    ssl = undefined;
+}
+
+const adapter = new PrismaPg({
+    connectionString:
+        process.env.DATABASE_URL,
+    ...(ssl && { ssl }),
 });
 
 const prisma = new PrismaClient({
