073c0d11a583857cef77a8bf141df08b264bb90c
Lavanya-Sinha <lavanyasinha400@gmail>
2026-08-21 00:51:48 +0530

    Configure Aiven PostgreSQL TLS

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import fs from "fs";
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false,
+        ca: fs.readFileSync(
+            "/etc/secrets/aiven-ca.pem",
+            "utf8"
+        ),
+        rejectUnauthorized: true,
     },
 });
 
